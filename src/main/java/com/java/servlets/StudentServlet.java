1a75a4a7320dcb8db53db7601de25d73b3627e6e
Rabindra Patra <RabindraPatra@corp.fairisaac.com>
2021-02-02 21:11:09 +0530

    Context docBase= | Servlet | debug | Servlet Terms

---
+++ src/main/java/com/java/servlets/StudentServlet.java
@@ -0,0 +1,19 @@
+package com.java.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class StudentServlet extends HttpServlet {
+    // GET
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        //super.doGet(req, resp);
+        // write own implementation
+        PrintWriter writer = resp.getWriter();
+        writer.write("welcome to webapp");
+    }
+}
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -1,5 +1,7 @@
 <html>
 <body>
 <h2>Hello World War Deployment!! Tomorrow we will see servlets</h2>
+<h2>We will learn servlets now </h2>
+<h2>Listener, Filter, Servlets</h2>
 </body>
 </html>
